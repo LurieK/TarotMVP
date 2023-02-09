2652b26d2875cd960b56148c9632bda9d1ddb247
LurieK <Lurainekimmerle@gmail.com>
2023-02-09 11:54:58 +0800

    first commit

---
+++ tarotmvp.js
@@ -0,0 +1,96 @@
+
+    cards = [
+        { Name: 'Fool', img: "./untitled folder/sm_RWSa-T-00.png" },
+        { Name: 'The Magician', img: './untitled folder/sm_RWSa-T-01.png' },
+        { Name: 'The High Priestess',img: "./untitled folder/sm_RWSa-T-02.png" },
+        { Name: 'The Emperor',img: "./untitled folder/sm_RWSa-T-03.png" }, 
+        { Name: 'The Empress',img: "./untitled folder/sm_RWSa-T-04.png" },
+        { Name: 'The Heirophant',img: "./untitled folder/sm_RWSa-T-05.png" },
+        { Name: 'The Lovers',img: "./untitled folder/sm_RWSa-T-06.png" },
+        { Name: 'The Chariot',img: "./untitled folder/sm_RWSa-T-07.png" },
+        { Name: 'Strength',img: "./untitled folder/sm_RWSa-T-08.png" },
+        { Name: 'The Hermit',img: "./untitled folder/sm_RWSa-T-09.png" },
+        { Name: 'Judgement',img: "./untitled folder/sm_RWSa-T-10.png" },
+        { Name: 'The Hanged Man',img: "./untitled folder/sm_RWSa-T-11.png" },
+        { Name: 'The Wheel of Fortune',img: "./untitled folder/sm_RWSa-T-12.png" },
+        { Name: 'Death',img: "./untitled folder/sm_RWSa-T-13.png" },
+        { Name: 'Temprence',img: "./untitled folder/sm_RWSa-T-14.png" }, 
+        { Name: 'The Devil',img: "./untitled folder/sm_RWSa-T-15.png" },
+        { Name: 'The Tower',img: "./untitled folder/sm_RWSa-T-16.png" },
+        { Name: 'The Star',img: "./untitled folder/sm_RWSa-T-17.png" },
+        { Name: 'The Moon',img: "./untitled folder/sm_RWSa-T-18.png" },
+        { Name: 'The Sun',img: "./untitled folder/sm_RWSa-T-19.png" },
+        { Name: 'Judgement',img: "./untitled folder/sm_RWSa-T-20.png"},
+        { Name: 'The World',img: "./untitled folder/sm_RWSa-T-21.png" },
+        { Name: 'King of Wands',img: "./untitled folder/sm_RWSa-W-KI.png" },
+        { Name: 'King of Cups',img: "./untitled folder/sm_RWSa-C-KI.png" },
+        { Name: 'King of Pentacles',img: "./untitled folder/sm_RWSa-KI-00.png" },
+        { Name: 'King of Swords',img: "./untitled folder/sm_RWSa-S-KI.png" },
+        { Name: 'Queen of Wands',img: "./untitled folder/sm_RWSa-W-QU.png" },
+        { Name: 'Queen of Cups',img: "./untitled folder/sm_RWSa-C-QU.png" },
+        { Name: 'Queen of Pentacles',img: "./untitled folder/sm_RWSa-P-QU.png" },
+        { Name: 'Queen of Swords',img: "./untitled folder/sm_RWSa-S-QU.png" },
+        { Name: 'Knight of Wands',img: "./untitled folder/sm_RWSa-W-J2.png" },
+        { Name: 'Knight of Cups',img: "./untitled folder/sm_RWSa-C-J2.png" },
+        { Name: 'Knight of Pentacles',img: "./untitled folder/sm_RWSa-P-J2.png" },
+        { Name: 'Knight of Swords',img: "./untitled folder/sm_RWSa-S-J2.png" },
+        { Name: 'Page of Wands',img: "./untitled folder/sm_RWSa-W-J1.png" },
+        { Name: 'Page of Cups',img: "./untitled folder/sm_RWSa-C-J1.png" },
+        { Name: 'Page of Pentacles',img: "./untitled folder/sm_RWSa-P-J1.png" },
+        { Name: 'Page of Swords',img: "./untitled folder/sm_RWSa-S-J1.png" },
+        { Name: 'Ten of Wands',img: "./untitled folder/sm_RWSa-W-10.png" },
+        { Name: 'Ten of Cups',img: "./untitled folder/sm_RWSa-C-10.png" },
+        { Name: 'Ten of Pentacles',img: "./untitled folder/sm_RWSa-S-10.png" },
+        { Name: 'Ten of Swords',img: "./untitled folder/sm_RWSa-S-10.png" },
+        { Name: 'Nine of Wands',img: "./untitled folder/sm_RWSa-W-09.png" },
+        { Name: 'Nine of Cups',img: "./untitled folder/sm_RWSa-C-09.png" },
+        { Name: 'Nine of Pentacles',img: "./untitled folder/sm_RWSa-P-09.png" },
+        { Name: 'Nine of Swords',img: "./untitled folder/sm_RWSa-S-09.png" },
+        { Name: 'Eight of Wands',img: "./untitled folder/sm_RWSa-W-08.png" },
+        { Name: 'Eight of Cups',img: "./untitled folder/sm_RWSa-C-08.png" },
+        { Name: 'Eight of Pentacles',img: "./untitled folder/sm_RWSa-P-08.png" },
+        { Name: 'Eight of Swords',img: "./untitled folder/sm_RWSa-S-08.png" },
+        { Name: 'Seven of Wands',img: "./untitled folder/sm_RWSa-W-07.png" },
+        { Name: 'Seven of Cups',img: "./untitled folder/sm_RWSa-C-07.png" },
+        { Name: 'Seven of Pentacles',img: "./untitled folder/sm_RWSa-P-07.png" },
+        { Name: 'Seven of Swords',img: "./untitled folder/sm_RWSa-S-07.png" },
+        { Name: 'Six of Wands',img: "./untitled folder/sm_RWSa-W-08.png" },
+        { Name: 'Six of Cups',img: "./untitled folder/sm_RWSa-C-08.png" },
+        { Name: 'Six of Pentacles',img: "./untitled folder/sm_RWSa-P-08.png" },
+        { Name: 'Six of Swords',img: "./untitled folder/sm_RWSa-S-08.png" },
+        { Name: 'Five of Wands',img: "./untitled folder/sm_RWSa-W-07.png" },
+        { Name: 'Five of Cups',img: "./untitled folder/sm_RWSa-C-07.png" },
+        { Name: 'Five of Pentacles',img: "./untitled folder/sm_RWSa-P-07.png" },
+        { Name: 'Five of Swords',img: "./untitled folder/sm_RWSa-S-07.png" },
+        { Name: 'Four of Wands',img: "./untitled folder/sm_RWSa-W-04.png" },
+        { Name: 'Four of Cups',img: "./untitled folder/sm_RWSa-C-04.png" },
+        { Name: 'Four of Pentacles',img: "./untitled folder/sm_RWSa-P-04.png" },
+        { Name: 'Four of Swords',img: "./untitled folder/sm_RWSa-S-04.png" },
+        { Name: 'Three of Wands',img: "./untitled folder/sm_RWSa-W-03.png" },
+        { Name: 'Three of Cups',img: "./untitled folder/sm_RWSa-C-03.png" },
+        { Name: 'Three of Pentacles',img: "./untitled folder/sm_RWSa-P-03.png" },
+        { Name: 'Three of Swords',img: "./untitled folder/sm_RWSa-S-03.png" },
+        { Name: 'Two of Wands',img: "./untitled folder/sm_RWSa-W-02.png" },
+        { Name: 'Two of Cups',img: "./untitled folder/sm_RWSa-C-02.png" },
+        { Name: 'Two of Pentacles',img: "./untitled folder/sm_RWSa-P-02.png" },
+        { Name: 'Two of Swords',img: "./untitled folder/sm_RWSa-S-02.png" },
+        { Name: 'Ace of Wands',img: "./untitled folder/sm_RWSa-W-0A.png" },
+        { Name: 'Ace of Cups',img: "./untitled folder/sm_RWSa-C-0A.png" },
+        { Name: 'Ace of Pentacles',img: "./untitled folder/sm_RWSa-P-0A.png" },
+        { Name: 'Ace of Swords',img: "./untitled folder/sm_RWSa-S-0A.png" }
+    ]
+        ;
+    
+  
+
+const button = document.querySelector(".button");
+const cardChoice = document.querySelector(".card-choice"); 
+
+
+
+button.addEventListener('click', function (){
+    let randomIndex = Math.floor(Math.random()*cards.length);
+    let randomCard = cards[randomIndex];
+    cardChoice.innerHTML = `<h3>${randomCard.Name}</h3><img src="${randomCard.img}" alt="${randomCard.Name}" style="display: block;">`
+});
+
